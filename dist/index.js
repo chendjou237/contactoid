ab472a4530ccc0ae1c40857d161571f42c1b6d39
chendjou237 <chenxhenor@gmail.com>
2023-08-01 23:47:02 +0100

    init

---
+++ dist/index.js
@@ -0,0 +1,9 @@
+import express from 'express';
+const app = express();
+app.get('/', (req, res) => {
+    res.send('Hello World! sd');
+});
+app.listen(3000, () => {
+    console.log('dying on that port port 3000');
+});
+//console.log('Hello Worlx d! sd sd');
